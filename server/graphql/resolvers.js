9e4dc9e54a70c8a65e1bbc01c1063963e785e985
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-02-11 23:11:06 -0500

    se agregaron la mutacion de crear projetcs y se agrego el query de consultar todos los projetcs

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,13 +1,22 @@
+import Project from "../models/Project.js";
 export const resolvers = {
     Query:{
-        hello: () => "Hello world!"
+        hello: () => "Hello world!",
+        project: async () => {
+            return await Project.find()
+        }
     },
 
 
+    /**Mutates in graphql*/
     Mutation:{
-        createProject: () =>{
-            return {};
+        createProject: async (_, {name, description}) =>{
+            const Project = new Project({
+                name,
+                description
+            });
+            const saveProject = await Project.save();
+            return saveProject;
         }
     }
-
 };
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -3,13 +3,14 @@ import { gql } from 'graphql-tag';
 export const typeDefs = gql `
     type Query {
         hello: String
+        project: [Project]
     }
 
     type Mutation {
-        createProject(name: String, description: String): Projecj
+        createProject(name: String, description: String): Project
     }
 
-    type Projecj {
+    type Project {
         _id: ID
         name: String
         description: String
