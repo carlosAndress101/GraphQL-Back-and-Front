7a49f0f5d1e69bc03aa3e1430a9826335aca41ed
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-02-23 21:39:52 -0500

    all resolvers and typedefs ready

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@ import { MONGODB_URI } from "./config.js";
 
 export const connecDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(MONGODB_URI);
     console.log(`Mongodb connect successfull: ${conn.connection.name}`);
   } catch {
