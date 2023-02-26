e2b929582582a5650fe2281bf8d213564f7834ab
carlosAndress101 <carlos.hinestrozap@unac.edu.co>
2023-02-26 00:30:19 -0500

    commit react estructura

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import {Projects} from './pages/Projects';
+import { ProjectDetails } from './pages/ProjectDetails'
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache:  new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div> Hello world</div>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Navigate to="/Projects"/>}/>
+      <Route path='/Projects' element={<Projects/>}/>
+      <Route path='/Projects/:id' element={<ProjectDetails/>}/>
+      </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
